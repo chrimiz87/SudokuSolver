a223120cb2c5329384019863a64c69a363211275
Chris Parkinson <cparkinson@mac-1U0-344.local>
2024-02-06 20:46:06 +0100

    Adding a first test via CMake

--- SudokuSolver.cc
+++ SudokuSolver.cc
@@ -2,10 +2,22 @@
 
 #include "Grid.hh"
 
-int main(){
+int main(int argc, char** argv){
+
+  if(argc>1){
+    // testing mode.
+
+    if( std::string(argv[1]).compare("Test1")==0){
+      // this is test 1 from CMake
+      Grid a;
+      a.printGridDimension();
+      a.printGridNSquares();
+      return 0;
+    }
+    
+  }
 
   Grid a;
-
   a.printGridDimension();
   a.printGridNSquares();
   a.printGrid();
