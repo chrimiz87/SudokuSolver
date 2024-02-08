9fcdb41085b5e0833581dc24eada4fa492c96e82
Chris Parkinson <cparkinson@mac-1U0-344.local>
2024-02-08 17:11:24 +0100

    Adding capability to set the value of a cell. Added a dedicated tester (SudokuTester) to the project.

--- source/Cell.cc
+++ source/Cell.cc
@@ -6,6 +6,12 @@ Cell::Cell() : possibles(9, true), nPossibles(9), solvedValue(0) {
 }
 
 bool Cell::checkValue(unsigned val){
+
+  // if the cell is already solved
+  if(nPossibles==1){
+    return (solvedValue == val);
+  }
+  
   unsigned tmp = val-1; // convert from 'user' 1-9 to vector index 0-8
   if(tmp>=0 && tmp<possibles.size()){
     return possibles[tmp];
@@ -13,4 +19,21 @@ bool Cell::checkValue(unsigned val){
   std::cout << " Warning. Tried to access 'possible' value " << val
 	    << " which is not in the range 1 -- " << possibles.size()+1 << std::endl;
   return false;
+}
+
+void Cell::setValue(unsigned val){
+
+  if(nPossibles==1 && solvedValue != val){
+    std::cout << " Warning Cell::setValue. tried to set value " << val
+	      << " but this cell is already solved with value " << solvedValue << std::endl;
+  }
+  
+  if(val==0 || val>9){
+    std::cout << " Warning in Cell::setValue. val " << val
+	      << " not valid, must be from 1-9 " << std::endl;
+    return; 
+  }
+  
+  nPossibles = 1;
+  solvedValue = val;
 }
